a077c37cda251a954fe0e7c37bc45b748c41c5a5
Ke Zheng-Xiang <kf_ke@hikingbook.net>
2025-06-17 12:14:19 +0800

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.1
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ProgressWebViewController",
-    platforms: [.iOS(.v14.5)],
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
