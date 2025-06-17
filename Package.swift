2092d00c5febcc510a762892ed7be1b5f65b845d
Ke Zheng-Xiang <kf_ke@hikingbook.net>
2025-06-17 12:16:57 +0800

    Exclude Info.plist

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ProgressWebViewController",
-            dependencies: [],
-            path: "ProgressWebViewController"),
+            path: "ProgressWebViewController",
+            exclude: ["Info.plist"]),
     ],
     swiftLanguageVersions: [.v5]
 )
